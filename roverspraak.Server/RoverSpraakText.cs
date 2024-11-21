b0916fa25ed0887a79ff5c9ac94da5e139ae2c47
arnst <arnstein@kongarthur.no>
2024-11-21 12:10:13 +0100

    Add Model for RoverspraakText

---
+++ roverspraak.Server/RoverSpraakText.cs
@@ -0,0 +1,11 @@
+﻿namespace roverspraak.Server
+{
+    public class RoverSpraakText
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public string? Alias { get; set; }
+        public string? OriginalText { get; set; }
+        public string? RoverText { get; set; }
+    }
+}
